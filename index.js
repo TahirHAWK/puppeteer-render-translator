0f60ec573f41720cedd02752d36669aeafe9b663
TahirHAWK <tahirtamin20@gmail.com>
2023-08-31 14:32:07 -0700

    some adjustments and optimizations

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const translate =  require('./translate')
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
+const path = require('path');
+const bodyParser = require('body-parser');
+
+// Parse JSON and URL-encoded data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('views'));
 
 app.get('/', (req, res) => {
-  res.send('Welcome to my custom translator api');
+  
+const filePath = path.join(__dirname, './views/home.html');
+  res.sendFile(filePath);
 });
 app.get('/translate/:sourceLang/:targetLang/:textToTranslate', translate.translateText)
+app.post('/translate', translate.translateTextForm)
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- translate.js
+++ translate.js
@@ -1,6 +1,5 @@
 const puppeteer = require('puppeteer');
 const path = require('path');
-require("dotenv").config();
 
 async function waitFor(time) {
   return new Promise((resolve) => {
@@ -10,11 +9,15 @@ async function waitFor(time) {
 
 
 exports.translateText = async function (req, res) {
-  let sourceLang, targetLang, sourceString
-  sourceString = req.params.textToTranslate
-  sourceLang = req.params.sourceLang
-  targetLang = req.params.targetLang
+  console.time('translate'); // Start the timer
 
+  let sourceLang, targetLang, sourceString
+  // sourceString = ' how are you doing? can we talk? I really need to tell you something. Do you even think about me?'
+  sourceString = req.params.textToTranslate
+  sourceLang =  req.params.sourceLang
+  targetLang =  req.params.targetLang
+console.log('the source string is: ', req.params)
+// how are you doing? can we talk? I really need to tell you something. Do you even think about me?
   if(sourceString.length > 4999){
     res.json('No can do amigo, string is too long.')
   } else{
@@ -22,18 +25,7 @@ exports.translateText = async function (req, res) {
   const filePath = path.join(__dirname, './', `.cache`, 'puppeteer', 'chrome', '115.0.5790.170-chrome-win64', 'chrome-win64', 'chrome.exe');
   let launchOptions = { headless: true, args: ['--start-maximized'], executablePath: filePath };
   // for online, no need to specify the path
-  // let launchOptions = {
-  //   args: [
-  //     "--disable-setuid-sandbox",
-  //     "--no-sandbox",
-  //     "--single-process",
-  //     "--no-zygote",
-  //   ],
-  //   executablePath:
-  //     process.env.NODE_ENV === "production"
-  //       ? process.env.PUPPETEER_EXECUTABLE_PATH
-  //       : puppeteer.executablePath(),
-  // };
+  // let launchOptions = { headless: true, args: ['--start-maximized'] };
 
   const browser = await puppeteer.launch(launchOptions);
   const page = await browser.newPage();
@@ -41,19 +33,17 @@ exports.translateText = async function (req, res) {
   // set viewport and user agent (just in case for nice viewing)
   await page.setViewport({ width: 1366, height: 768 });
   await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
-
- await page.goto(`https://translate.google.com/?sl=${sourceLang}&tl=${targetLang}&text=${sourceString}&op=translate`);
+    
+  await page.goto(`https://translate.google.com/?sl=${sourceLang}&tl=${targetLang}&text=${sourceString}&op=translate`);
 
   // detect the source textarea for input data (source string)
   await page.waitForSelector('.er8xn');
-  await waitFor(1000);
+  // await waitFor(1000);
 
-  // type the source string on the textarea
-  // await page.type('.er8xn', sourceString);
 
   // wait for the result container to be available
   await page.waitForSelector('.usGWQd');
-  await waitFor(3000);
+  // await waitFor(3000);
 
   // get the translated result string
   let translatedResult = await page.evaluate(() => {
@@ -63,10 +53,20 @@ exports.translateText = async function (req, res) {
   // display the source and translated text to the console
   console.log(`${sourceLang}: ${sourceString}\n${targetLang}: ${translatedResult}`);
   
-  await waitFor(1000);
+  // await waitFor(1000);
   await browser.close();
   res.json(translatedResult) 
+  // return translatedResult
   }
 
+  console.timeEnd('translate'); 
   
+}
+
+exports.translateTextForm = function(req, res){
+  console.log(req.body)
+
+  res.redirect(`/translate/en/de/${encodeURIComponent(req.body.textToTranslate)}`)
+
+
 }
